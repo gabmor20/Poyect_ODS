f4f4a2c34f8dd4210c18b99c791d3a1beaa706ed
Edna Gabriela Moron Sanchez <gabmor@sfincomercio.local>
2026-02-07 15:27:44 -0500

    conexión a base dedatos posgresSQL en la nube neon

--- src/infaestructure/routes/UserRoutes.ts
+++ src/infaestructure/routes/UserRoutes.ts
@@ -13,7 +13,7 @@ const userController = new UserController(userApp);
 
 //definición de las rutas
 
-router.post("/users", async (req, res)=>{
+router.post("/", async (req, res)=>{
     try {
         await userController.createUser(req, res);
 
@@ -21,10 +21,11 @@ router.post("/users", async (req, res)=>{
         res.status(500).json({message: "Error en la creación de usuario", error});
     }
 });
-router.get("/users", async(req, res)=>{
+router.get("/", async(req, res)=>{
     try {
         await userController.getAllUsers(req, res);
     } catch (error) {
         res.status(500).json({message:"Error en la consulta de datos", error});
     }
 })
+export default router;
--- src/infaestructure/web/app.ts
+++ src/infaestructure/web/app.ts
@@ -1,17 +1,23 @@
 import express, {type Request, type Response} from "express";
+import userRoutes from "../routes/UserRoutes"; 
 
-class App{
+
+  class App{
     private app: express.Application;
 
     constructor(){
         this.app = express();
+        this.app.use(express.json()); 
         this.routes();
     }
 
-    private routes(): void{
-        this.app.get("/", (req: Request, res: Response)=> res.send("Hello World!"));
-         this.app.get("/health", (req: Request, res: Response)=> res.send("Health check OK!"));
-    }
+   private routes(): void {
+    this.app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+    this.app.get("/health", (req: Request, res: Response) => res.send("Health check OK!"));
+
+
+    this.app.use("/users", userRoutes); 
+}
 
     getApp(){
         return this.app;
